af68acc39722a607a65b43666de531b6621e6aad
bjkeulen <39900544+bjkeulen@users.noreply.github.com>
2026-02-27 15:52:14 +0100

    Update license statement

--- plot/plotSetup.m
+++ plot/plotSetup.m
@@ -1,7 +1,7 @@
 % Author: B.J. Keulen
 % Date: 25-10-2024
 %
-% Copyright 2025 B.J. Keulen and M.J. Stam
+% Copyright 2026 B.J. Keulen, M.J. Stam & J.T. Boonstra
 % SPDX-License-Identifier: Apache-2.0
 %
 % Function for plotting data from dataSetup, the output from the getSetup
